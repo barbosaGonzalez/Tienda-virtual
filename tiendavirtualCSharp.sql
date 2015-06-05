2739d89f167ac331337c5462be4b283209e196cc
Edsel Barbosa Gonzalez <barbosa_gonzalez@outlook.com>
2015-06-05 09:50:40 -0500

    Actualización
    Actualización

--- tiendavirtualCSharp.sql
+++ tiendavirtualCSharp.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Base de datos: `tiendaonline`
 --
 CREATE DATABASE IF NOT EXISTS `tiendavirtualCSharp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `tiendaonline`;
+USE `tiendavirtualCSharp`;
 
 -- --------------------------------------------------------
 
